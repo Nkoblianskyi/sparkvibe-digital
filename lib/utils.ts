bbec76e4f2865b96316a11ad6824f8cb66983179
Nykyta Koblianskyi <nkoblianskyi@gmail.com>
2025-05-15 08:54:57 +0200

    img

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,17 +4,17 @@ import { twMerge } from "tailwind-merge"
 export const getServiceImage = (title: string): string => {
   switch (title) {
     case "SEO Services":
-      return "/placeholder-g3tys.png"
+      return "/65ca8b99f19ae295c7064e13733c6872.jpg"
     case "PPC Services":
-      return "/placeholder-gl9ax.png"
+      return "/df2ada68e83aa79dbef3ffa297f30c84.jpg"
     case "Affiliate Services":
-      return "/placeholder-m83wn.png"
+      return "/e0c9cc90920a39f5619cf78e6482ea6b.jpg"
     case "Content Services":
-      return "/placeholder-pa4z7.png"
+      return "/ef81f8bb773eba451c3c4f69ba57717a.jpg"
     case "Social Media Services":
-      return "/placeholder-k4exm.png"
+      return "/b134c8e471a4dc4c8ba40b2f75afbfa9.jpg"
     case "Digital Marketing":
-      return "/placeholder-6jgs6.png"
+      return "/62c709c76650c2e902d16565a5b33657.jpg"
     default:
       return "/digital-services-concept.png"
   }
